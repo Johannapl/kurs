c238bb1e11075c4f016b0781d4f8d2f79a9680f4
Johannapl <johanna.lahteenmaki@gmail.com>
2025-09-16 23:14:42 +0200

    Add files via upload

---
+++ Fra_C_til_F.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 22:32:12 2025
+@author: Johanna
+
+"""
+
+F = 0 # [farhenheit]
+C = (F-32)/1.8 # (celsius)
+print ('F =',F)
+print('C =',C)
+
+      
+
