5bdebf1fe79121ab166c8815f0cbd56b42f8d39c
Johannapl <johanna.lahteenmaki@gmail.com>
2025-11-11 19:28:12 +0100

    Add files via upload

---
+++ arbeidskrav2_oppg1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 23:24:57 2025
+
+@author: Johanna
+
+"""
+import math
+# Tar inn fødselsår fra bruker. #
+alder = int(input("Hvilket år er du født:"))
+alder_2024= 2024 - alder
+# Skriver ut hvor gammel personen blir i løpet av 2024. #
+print(f"Personen blir {alder_2024} i 2024")
+
+ 
