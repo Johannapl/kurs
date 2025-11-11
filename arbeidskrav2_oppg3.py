330b18da3b0c59af469d5660f575f2c394b4854f
Johannapl <johanna.lahteenmaki@gmail.com>
2025-11-11 19:33:47 +0100

    Add files via upload

---
+++ arbeidskrav2_oppg3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 22:32:12 2025
+@author: Johanna
+
+"""
+
+import numpy as np
+# Tar inn antall grader fra brukeren. #
+v_grad = float(input('Skriv inn gradtallet:'))
+v_rad = v_grad*np.pi/180
+# Skriver ut resultatet i radianer. #
+print(v_grad, "grader er", v_rad, "radianer")
+
+
+
+
+
+
+      
+
